93857465a9349074b060879b3a11d4a31109d983
Thrauk <stoicath@gmail.com>
2021-11-01 11:59:24 +0200

    Laboratorul 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:laborator_sma/lib/src/battery_display.dart';
 
 import 'lib/src/web_view_app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const BatteryDisplay());
 }
